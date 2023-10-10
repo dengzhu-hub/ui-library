53d09d97ba15573ee2ad48a53da9774e7c7641cb
dengzhu-hub <3052113768@qq.com>
2023-10-10 12:09:17 +0800

    style css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-type Props = {
-    text: string;
+import './Button.css'
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?:'primary' | "secondary";
 }
-const Button:React.FC<Props> = ({text}) => {
+const Button:React.FC<ButtonProps> = ({variant, ...props}) => {
+  const styles = () => {
+    const propStyle = props.style ??{};
+    if(props.disabled) return propStyle;
+    if(variant === 'primary') return { borderColor: '#087f5b', backgroundColor: '#20c997', color: '#f03e3e', ...propStyle };
+    if (variant==='secondary') return { borderColor: '#343a40', backgroundColor: '#adb5bd', color: '#fab005', ...propStyle };
+    return propStyle;
+  }
   return (
-    <div>Button</div>
+   <button style={styles()} {...props}>{props.children}</button>
   )
 }
 
